cddcb047f8e5f2b5d423896af71179873221b7f9
Fábio Valentim <maitabom@gmail.com>
2025-09-21 20:57:59 -0300

    adiciona enum AuthServiceType e factory para instanciar AuthMockService

--- lib/services/auth/auth_service.dart
+++ lib/services/auth/auth_service.dart
@@ -1,6 +1,9 @@
 import 'dart:io';
 
 import 'package:chat/models/chat_user.dart';
+import 'package:chat/services/auth/auth_mock_service.dart';
+
+enum AuthServiceType { mock }
 
 abstract class AuthService {
   ChatUser? get currentUser;
@@ -9,4 +12,11 @@ abstract class AuthService {
   Future<void> signUp(String name, String email, String password, File? image);
   Future<void> login(String email, String password);
   Future<void> logout();
+
+  factory AuthService(AuthServiceType type) {
+    switch (type) {
+      case AuthServiceType.mock:
+        return AuthMockService();
+    }
+  }
 }
