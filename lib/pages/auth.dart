190911e377be8fea452eff021ead1f7a4f160211
Fábio Valentim <maitabom@gmail.com>
2025-09-21 20:35:42 -0300

    corrige a lógica de autenticação na AuthPage para incluir o tratamento de login e cadastro

--- lib/pages/auth.dart
+++ lib/pages/auth.dart
@@ -1,5 +1,6 @@
 import 'package:chat/components/auth_form.dart';
 import 'package:chat/models/auth_form_data.dart';
+import 'package:chat/services/auth/auth_mock_service.dart';
 import 'package:flutter/material.dart';
 
 class AuthPage extends StatefulWidget {
@@ -19,7 +20,15 @@ class _AuthPageState extends State<AuthPage> {
       });
 
       if (formData.isLogin) {
-      } else {}
+        await AuthMockService().login(formData.email, formData.password);
+      } else {
+        await AuthMockService().signUp(
+          formData.name,
+          formData.email,
+          formData.password,
+          formData.image,
+        );
+      }
     } catch (error) {
       //Tratar error
     } finally {
