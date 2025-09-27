ebb6ae426913259d20fb566c7dd2b6aa47961643
Fábio Valentim <maitabom@gmail.com>
2025-09-27 19:17:16 -0300

    refatora MessageBubble para melhorar a exibição de mensagens e adicionar suporte a avatares de usuários

--- lib/components/message_bubble.dart
+++ lib/components/message_bubble.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:chat/models/chat_message.dart';
 import 'package:flutter/material.dart';
 
@@ -11,39 +13,75 @@ class MessageBubble extends StatelessWidget {
     super.key,
   });
 
+  Widget _showUserAvatar(String imageUrl) {
+    ImageProvider? provider;
+    final uri = Uri.parse(imageUrl);
+    final assetImage = 'assets/avatar.png';
+
+    if (uri.path.contains(assetImage)) {
+      provider = AssetImage(assetImage);
+    } else if (uri.scheme.contains('http')) {
+      provider = NetworkImage(uri.toString());
+    } else {
+      provider = FileImage(File(uri.toString()));
+    }
+
+    return CircleAvatar(backgroundImage: provider);
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Row(
-      mainAxisAlignment: fromCurrentUser
-          ? MainAxisAlignment.end
-          : MainAxisAlignment.start,
+    return Stack(
       children: [
-        Container(
-          decoration: BoxDecoration(
-            color: fromCurrentUser
-                ? Colors.grey.shade300
-                : Theme.of(context).colorScheme.secondary,
-            borderRadius: BorderRadius.all(Radius.circular(12)),
-          ),
-          width: 180,
-          padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 16),
-          child: Column(
-            children: [
-              Text(
-                message.userName,
-                style: TextStyle(
-                  fontWeight: FontWeight.bold,
-                  color: fromCurrentUser ? Colors.black : Colors.white,
+        Row(
+          mainAxisAlignment: fromCurrentUser
+              ? MainAxisAlignment.end
+              : MainAxisAlignment.start,
+          children: [
+            Container(
+              decoration: BoxDecoration(
+                color: fromCurrentUser
+                    ? Colors.grey.shade300
+                    : Theme.of(context).colorScheme.secondary,
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(12),
+                  topRight: Radius.circular(12),
+                  bottomLeft: fromCurrentUser
+                      ? Radius.circular(12)
+                      : Radius.zero,
+                  bottomRight: fromCurrentUser
+                      ? Radius.zero
+                      : Radius.circular(12),
                 ),
               ),
-              Text(
-                message.text,
-                style: TextStyle(
-                  color: fromCurrentUser ? Colors.black : Colors.white,
-                ),
+              width: 180,
+              padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 16),
+              margin: EdgeInsets.symmetric(vertical: 15, horizontal: 8),
+              child: Column(
+                children: [
+                  Text(
+                    message.userName,
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      color: fromCurrentUser ? Colors.black : Colors.white,
+                    ),
+                  ),
+                  Text(
+                    message.text,
+                    style: TextStyle(
+                      color: fromCurrentUser ? Colors.black : Colors.white,
+                    ),
+                  ),
+                ],
               ),
-            ],
-          ),
+            ),
+          ],
+        ),
+        Positioned(
+          top: 0,
+          left: fromCurrentUser ? null : 165,
+          right: fromCurrentUser ? 165 : null,
+          child: _showUserAvatar(message.userImagemUrl),
         ),
       ],
     );
