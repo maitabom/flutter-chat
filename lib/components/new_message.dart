bc2f59b1b179e4b3d2aa81af45282df219ee085c
Fábio Valentim <maitabom@gmail.com>
2025-09-27 19:50:39 -0300

    refatora NewMessage para gerenciar o estado e enviar mensagens de chat

--- lib/components/new_message.dart
+++ lib/components/new_message.dart
@@ -1,10 +1,49 @@
+import 'package:chat/services/auth/auth_service.dart';
+import 'package:chat/services/chat/chat_service.dart';
 import 'package:flutter/material.dart';
 
-class NewMessage extends StatelessWidget {
+class NewMessage extends StatefulWidget {
   const NewMessage({super.key});
 
   @override
+  State<NewMessage> createState() => _NewMessageState();
+}
+
+class _NewMessageState extends State<NewMessage> {
+  String _enteredMessage = '';
+
+  final _messageController = TextEditingController();
+
+  final authService = AuthService(AuthServiceType.mock);
+  final chatService = ChatService(ChatServiceType.mock);
+
+  Future<void> _sendMessage() async {
+    final user = authService.currentUser;
+
+    if (user != null) {
+      await chatService.save(_enteredMessage, user);
+      _messageController.clear();
+    }
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Center(child: Text('Nova Mensagem'));
+    return Row(
+      children: [
+        Expanded(
+          child: TextField(
+            controller: _messageController,
+            onChanged: (message) => setState(() {
+              _enteredMessage = message;
+            }),
+            decoration: InputDecoration(labelText: 'Enviar mensagem'),
+          ),
+        ),
+        IconButton(
+          onPressed: _enteredMessage.trim().isEmpty ? null : _sendMessage,
+          icon: Icon(Icons.send),
+        ),
+      ],
+    );
   }
 }
